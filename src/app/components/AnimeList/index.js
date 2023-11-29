82305a528cf415b7b97776d5a29a1f0ac2e49942
junaaxy <arjunabangka12@gmail.com>
2023-11-29 17:44:27 +0700

    add fitur

--- src/app/components/AnimeList/index.js
+++ src/app/components/AnimeList/index.js
@@ -4,7 +4,7 @@ import Link from "next/link";
 const AnimeList = ({title, images,id}) => {
     return (
             <Link href={`/${id}`} className=" cursor-pointer">  
-                <Image src={images} alt="" width={350} height={350}/>
+                <Image src={images} alt="" width={350} height={350} className=" w-full md:bg-cover md:max-h-full 2xl:object-cover lg:max-h-80 2xl:max-h-96 2xl:w-full "/>
                 <p className="p-4 md:text-xl lg:text-xl xl:text-xl text-md  font-bold">Tittle : {title}</p>
             </Link>
     )
--- src/app/page.js
+++ src/app/page.js
@@ -6,15 +6,15 @@ const response = await fetch (`${process.env.NEXT_PUBLIC_API_BASE_URL}/top/anime
 const anime = await response.json()
  return (
   <div>
-    <div className="p-2 flex justify-between">
+    <div className="p-2 flex justify-between items-center">
       <h1 className="font-semibold">Top Anime</h1>
-      <Link href="/" className="text-sm font-bold underlinehover:text-zinc-700 transition-all">Lihat Semua</Link>
+      <Link href="/populer" className="text-sm font-bold underlinehover:text-zinc-700 transition-all">Lihat Semua</Link>
     </div>
     <div className="grid md:grid-cols-4 sm:grid-cols-3 grid-cols-2 gap-4 px-2"> 
     {anime.data.map(
       data => {
        return (
-       <div key={data.mal_id} className="shadow-xl">
+       <div key={data.mal_id} className="shadow-xl 2xl:w-96 2xl:px-10 ">
         <AnimeList title={data.title} images={data.images.webp.image_url} id={data.mal_id}/>
        </div>
        ) 
